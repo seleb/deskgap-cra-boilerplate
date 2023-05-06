74df886ed87617395333e160a6064680f9f273ac
Sean S. LeBlanc <sean.s.leblanc@gmail.com>
2023-05-06 10:22:15 -0400

    start deskgap script

--- scripts/build.js
+++ scripts/build.js
@@ -1,4 +1,6 @@
 const child_process = require('child_process');
+const path = require('path');
+const pkg = require('../package.json');
 
 child_process.execSync('npm run build:react', {
 	stdio: 'inherit',
@@ -7,3 +9,8 @@ child_process.execSync('npm run build:deskgap', {
 	stdio: 'inherit',
 });
 console.log('👍');
+if (process.argv.slice(2)[0] === 'open') {
+	child_process.execSync(path.resolve(__dirname, `../bin/${pkg.name}.exe`), {
+		stdio: 'inherit',
+	});
+}
