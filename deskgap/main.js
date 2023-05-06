acb63650f81fc00f65616641c429d2bf59a54d45
Sean S. LeBlanc <sean.s.leblanc@gmail.com>
2023-05-06 12:05:47 -0400

    cleanup promise chain

--- deskgap/main.js
+++ deskgap/main.js
@@ -1,8 +1,4 @@
-const {
-	app,
-	BrowserWindow,
-	messageNode,
-} = require('deskgap');
+const { app, BrowserWindow, messageNode } = require('deskgap');
 const pkg = require('./package.json');
 const createServer = require('./server');
 
@@ -20,19 +16,18 @@ const win = new BrowserWindow({
 // setup server + load index in window when it's ready
 const hostname = 'localhost';
 let server;
-Promise.all([
-	new Promise(resolve => app.once('ready', resolve)),
-	new Promise(resolve => server = createServer(0, hostname, resolve)),
-]).then(() => {
-	win.webContents.executeJavaScript(`console.log('started')`);
-	win.webContents.executeJavaScript(`console.log(navigator.userAgent)`);
-	const url = `http://${hostname}:${server.address().port}`;
-	win.webContents.executeJavaScript(`console.log('${url}')`);
-	return win.loadURL(url);
-}).then(() => {
-	win.webContents.executeJavaScript(`console.log('loaded')`);
-}).catch((err) => {
-	console.error(err);
-	win.webContents.executeJavaScript(`console.log('failed')`);
-	win.webContents.executeJavaScript(`console.log(\`${err.toString()}\`)`);
-});
+new Promise(resolve => (server = createServer(0, hostname, resolve)))
+	.then(() => app.whenReady())
+	.then(() => {
+		win.webContents.executeJavaScript(`console.log('started')`);
+		win.webContents.executeJavaScript(`console.log(navigator.userAgent)`);
+		const url = `http://${hostname}:${server.address().port}`;
+		win.webContents.executeJavaScript(`console.log('${url}')`);
+		return win.loadURL(url);
+	})
+	.then(() => win.webContents.executeJavaScript(`console.log('loaded')`))
+	.catch(err => {
+		console.error(err);
+		win.webContents.executeJavaScript(`console.log('failed')`);
+		win.webContents.executeJavaScript(`console.log(\`${err.toString()}\`)`);
+	});
